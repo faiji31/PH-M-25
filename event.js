841065d5fc4838a19f3ec4868ccb040ebe8a9414
Md.Faiji Akbar <faijiakbarliam2016@gmail.com>
2025-08-24 00:11:51 +0600

    4th commit

---
+++ event.js
@@ -0,0 +1,9 @@
+ document.getElementById('btn-update-title').addEventListener('click',function(){
+            const pageTitle = document.getElementById('page-title');
+            pageTitle.innerText = "I am Update";
+            
+        })
+        document.getElementById("login-btn").addEventListener('click',function(){
+            const login = document.getElementById('login');
+            login.innerText = "Login Successful";
+        })
