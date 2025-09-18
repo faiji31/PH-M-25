2e864fe2dae8b0fbf40384f76873cff6f9e82764
Md.Faiji Akbar <faijiakbarliam2016@gmail.com>
2025-09-18 19:31:21 +0600

    3rd commit

--- liam.js
+++ liam.js
@@ -1,1 +1,13 @@
-console.log('connected')
+// console.log('connected')
+function makeYellow(){
+    document.body.style.backgroundColor = 'yellow';
+}
+
+function makeBlue(){
+    document.body.style.backgroundColor = 'blue';
+}
+
+const makeBlue = document.getElementById('btn-makeblue');
+makeBlue.onClick  = function Blue(){
+    document.body.style.backgroundColor = 'red';
+}
