79d05e22177d5bc6692f6950ae71ff4bf1ea77d2
Md.Faiji Akbar <faijiakbarliam2016@gmail.com>
2025-09-19 19:38:04 +0600

    6th commit

--- handler.js
+++ handler.js
@@ -1,1 +1,5 @@
-console.log('handeler file added')
+document.getElementById('btn-update-title').addEventListener('click',function(){
+    const title = document.getElementById('title-text');
+    console.log(title)
+    title.innerText = "text updated";
+})
